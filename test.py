c052a4127dbe65b00baf69cdfcbf9ddcf4dca8b3
Stefan M�hlbauer <smba@users.noreply.github.com>
2024-05-11 19:51:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,35 @@
+# Import required libraries and modules
 import numpy as np
-
 from gsnoop.util import diff_transform, xor_transform
 from gsnoop.screening import group_screening, lasso_screening
 from gsnoop.causal import find_hitting_set
 
+# Set the random seed for reproducibility
 np.random.seed(1)
 
-# Specify problem space
+# Define the size of the problem space
 n_features = 50
 n_configs = 50
 
-# Specify simple performance oracle
-func = lambda x: x[0] * x[1] * 123 + x[3] * 45 + x[4] * x[5] * 67 + 0.01
+# Define a simple performance function to simulate system behavior
+# The function models the performance of a system given a configuration of features
+def performance_oracle(x):
+    return x[0] * x[1] * 123 + x[3] * 45 + x[4] * x[5] * 67 + 0.01
 
-# Draw random sample, compute performance
+# Generate random configurations and evaluate performance using the defined oracle
 x = np.random.choice(2, size=(n_configs, n_features))
-y = np.array(list(map(func, x)))
+y = np.array(list(map(performance_oracle, x)))
 
-# baseline
+# Conduct baseline screening using LASSO
 lasso_options = lasso_screening(x, y)
-print(lasso_options)
+print("Lasso Screening Results:", lasso_options)
 
-# Perform stepwise 'group screening'
-x_, y_ = diff_transform(x, y)
-group_options = group_screening(x_, y_)
-print(group_options)
-# print(group_options)
-# > [0, 1, 2, 3, 4, 5]
+# Perform group screening using the difference transformation
+x_diff_transformed, y_diff_transformed = diff_transform(x, y)
+group_options = group_screening(x_diff_transformed, y_diff_transformed)
+print("Group Screening Results:", group_options)
 
-# Perform causal group screening
-x_,y_ = xor_transform(x, y)
-causal_options = find_hitting_set(x_, y_)
-print(causal_options)
-# print(causal_options)
-# > [0, 1, 2, 3, 4, 5]
+# Perform causal analysis using the XOR transformation
+x_xor_transformed, y_xor_transformed = xor_transform(x, y)
+causal_options = find_hitting_set(x_xor_transformed, y_xor_transformed)
+print("Causal Screening Results:", causal_options)
