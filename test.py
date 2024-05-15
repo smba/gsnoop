512e895e3e6b5afa4648a3fdb17952168b563f86
Stefan Mühlbauer <muehlbauer@informatik.uni-leipzig.de>
2024-05-15 09:14:14 +0200

    ber

--- test.py
+++ test.py
@@ -22,21 +22,26 @@ x = np.random.choice(2, size=(n_configs, n_features))
 y = np.array(list(map(performance_oracle, x)))
 
 # Conduct baseline screening using LASSO
+print("> ", "Running Lasso Screening...")
 lasso_options = lasso_screening(x, y)
-print("Lasso Screening Results:", lasso_options)
+print("> ", f"Selected {len(lasso_options)} of {n_features} features.\n")
 
 # Perform group screening using the difference transformation
 x_diff_transformed, y_diff_transformed = diff_transform(x, y)
+print("> ", "Running Group Screening...")
 group_options = group_screening(x_diff_transformed, y_diff_transformed)
-print("Group Screening Results:", group_options)
+print("> ", f"Selected {len(group_options)} of {n_features} features.\n")
 
 # Perform group screening using the difference transformation
 x_diff_transformed, y_diff_transformed = diff_transform(x, y)
+print("> ", "Running Stepwise Screening...")
 stepwise_options = stepwise_screening(x_diff_transformed, y_diff_transformed)
-print("Stepwise Screening Results:", group_options)
+print("> ", f"Selected {len(stepwise_options)} of {n_features} features.\n")
+
 
 x_xor_transformed, y_xor_transformed = xor_transform(x, y)
 x_xor_transformed = np.vstack([x_xor_transformed[i,:] for i in range(x_xor_transformed.shape[0]) if y_xor_transformed[i] != 0])
+print("> ", "Running Hochbaum's MHS Approximation...")
 causal_options = find_greedy_hitting_set(x_xor_transformed)
-print("MHS solving using Hochbaum's Approximation", causal_options)
+print("> ", f"Selected {len(causal_options)} of {n_features} features.\n")
 
